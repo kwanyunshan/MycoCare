d19129a0e809d221df5197aeef87d9f74e91e30c
kwanyunshan <yunshan0503@gmail.com>
2026-08-22 13:08:01 +0800

    update java script

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-fetch("http://192.168.0.101/data")
+function getSensorData() {
 
-.then(response=>response.json())
+    fetch("get_data.php")
 
-.then(data=>{
+        .then(response => response.json())
 
-document.getElementById("temp").innerHTML=data.temperature;
+        .then(data => {
 
-});
+            document.getElementById("temperature").innerText =
+                parseFloat(data.temperature).toFixed(1) + " °C";
+
+            document.getElementById("humidity").innerText =
+                parseFloat(data.humidity).toFixed(1) + " %";
+
+            document.getElementById("airQuality").innerText =
+                data.air_quality;
+
+        })
+
+        .catch(error => {
+            console.error("Error:", error);
+        });
+}
+
+
+// Immediately get data
+getSensorData();
+
+
+// Update Dashboard every 2 seconds
+setInterval(getSensorData, 2000);
