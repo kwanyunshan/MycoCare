9a76fb5e91d2ac61149085097a68e4361c6a6c24
kwanyunshan <yunshan0503@gmail.com>
2026-09-07 22:59:33 +0800

    remove success message from database connection and unused ventilation fan section from dashboard

--- db_connect.php
+++ db_connect.php
@@ -10,7 +10,4 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
-
-echo "Database connected successfully!";
-
 ?>
